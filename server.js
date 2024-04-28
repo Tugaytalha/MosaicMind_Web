cc8553d42fac1c463ca48d855da2d26d067ca88e
Tugay Talha İçen <76882910+Tugaytalha@users.noreply.github.com>
2024-04-29 01:03:51 +0300

    Error handler, axios and some more feature added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
+const { initializeApp } = require('firebase/app');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.set("view engine", "ejs");
 
